a5ad085f4dad761a6254521577211fac4968b5a8
longwatermelon <longwatermelon1@gmail.com>
2023-05-29 15:20:32 -0700

    web demo

---
+++ emscripten.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+em++ -sUSE_SDL=2 -std=c++17 main.cpp -o docs/index.html
--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <array>
 #include <complex>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
+
 using Im = std::complex<float>;
 using namespace std::complex_literals;
 
@@ -27,29 +31,27 @@ Im c_n(const std::vector<Im> &f, int n)
     return c;
 }
 
-int main(int argc, char **argv)
+SDL_Window *win;
+SDL_Renderer *rend;
+
+bool image_drawn = false;
+bool mouse_down = false;
+std::vector<Im> f;
+
+int range = 100;
+int N = 1 + range * 2;
+std::vector<Im> vc(N);
+bool vc_calculated = false;
+
+float t = 0.f;
+
+void run()
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *win = SDL_CreateWindow("Fourier draw",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        600, 600, SDL_WINDOW_SHOWN);
-    SDL_Renderer *rend = SDL_CreateRenderer(win, -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-    bool image_drawn = false;
-    bool mouse_down = false;
-    std::vector<Im> f;
-
-    int range = 100;
-    int N = 1 + range * 2;
-    std::vector<Im> vc(N);
-    bool vc_calculated = false;
-
-    float t = 0.f;
-
     bool running = true;
     SDL_Event evt;
+#ifndef __EMSCRIPTEN__
     while (running)
+#endif
     {
         while (SDL_PollEvent(&evt))
         {
@@ -87,7 +89,11 @@ int main(int argc, char **argv)
         }
 
         if (image_drawn)
+#ifdef __EMSCRIPTEN__
+            t += 1.f / f.size();
+#else
             t += 5.f / f.size();
+#endif
 
         SDL_RenderClear(rend);
 
@@ -135,6 +141,22 @@ int main(int argc, char **argv)
         SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
         SDL_RenderPresent(rend);
     }
+}
+
+int main(int argc, char **argv)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    win = SDL_CreateWindow("Fourier draw",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        600, 600, SDL_WINDOW_SHOWN);
+    rend = SDL_CreateRenderer(win, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(run, -1, 1);
+#else
+    run();
+#endif
 
     SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
